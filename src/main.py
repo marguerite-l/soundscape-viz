14745604ee9f712d2f1d962aea67a2da45e3e6f9
marguerite-l <m_luu@live.concordia.ca>
2026-05-07 11:50:29 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,6 +4,10 @@ from scipy.io import wavfile
 # Load audio
 rate, data = wavfile.read("data/XC924056 - Savannah Sparrow - Passerculus sandwichensis.wav")
 
+# Info about the audio file
+print("Sample rate:", rate)
+print("Audio shape:", data.shape)  #(total samples, channels)
+
 # Create spectrogram
 plt.figure(figsize=(10, 4))
 plt.specgram(data, Fs=rate)
